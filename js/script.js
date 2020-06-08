cc807700b736ff57e38e836ab23117a75e7ff039
ghost01123 <ghost011@protonmail.com>
2020-06-08 01:16:19 +0000

    script update - victory + clean up

--- js/script.js
+++ js/script.js
@@ -1,40 +1,47 @@
 // init-1/ define var's
 const cards = Array.from(document.querySelectorAll('.m-card'));
+cardsArray = cards;
 let hasFlippedCard = false;
 let lockBoard = true;
 let firstCard, secondCard;
 matchedCards = [];
+let N = 16;
 let bMusic;
 let flipSound;
-let Successs;
+let win;
 let g0c ;
 let o0f;
 ticker = document.getElementById('info-span-2f');
 timer = document.getElementById('info-span-1rem');
 let totalClicks = 0;
-totalTime = 5;
+totalTime = 89;
 timeRemaining = totalTime;
 ticker.innerText = 0;
 lossOverlay = document.getElementById('game-over');
+victoryOverlay = document.getElementById('victory');
+//cursor
+let elementForCur = document.getElementsByTagName("body")[0];
+elementForCur.style.cursor = "url('assets/cur/ani547.cur'), url('nezuko-kamado.cur'), auto";
 
 // preload the audio
 (function preload() {
   bMusic = new Audio('assets/sounds/Kiss-of-death.mp3');
   flipSound = new Audio('assets/sounds/flip.wav');
-  Successs = new Audio('assets/sounds/well-done.wav');
   o0f = new Audio('assets/sounds/oof.wav');
   bMusic.volume = 0.21;
   bMusic.loop = true;
 })();
-//game over audio
-function goc(params) {
+//game over + victory audio - load
+function goc() {
   g0c = new Audio('assets/sounds/game-over.wav');
   g0c.play();
 }
 
 function init() {
-  ticker.innerText = totalClicks;
+  // the main event handler
+  cards.forEach(card => card.addEventListener('click', flipCard));
   totalClicks = 0;
+  ticker.innerText = totalClicks;
   lockBoard = false;
   bMusic.play();
   let CDInterval = setInterval(countDown, 1000);
@@ -43,7 +50,7 @@ function init() {
     timer.innerText = timeRemaining;
       timeRemaining--;
       // if game over
-      if(timeRemaining === 0){
+      if(timeRemaining === 0) {
       clearInterval(CDInterval);
           gameOver();
       }
@@ -67,7 +74,6 @@ function flipCard() {
   hasFlippedCard = false;
   secondCard = this;
   checkForMatch();
-
 }
 
 function checkForMatch() {
@@ -80,9 +86,11 @@ function disableCards() {
   firstCard.removeEventListener('click', flipCard);
   secondCard.removeEventListener('click', flipCard);
   matchedCards ++;
-
   console.log(matchedCards);
   resetBoard();
+  if (matchedCards === N /2) {
+    succsessScreen();
+  }
 }
 
 function unflipCards() {
@@ -110,25 +118,36 @@ function gameOver() {
 
 //reset timer + flips
 function restart() {
-  
+  lossOverlay.classList.remove('show');
+  victoryOverlay.classList.remove('show');
+  totalTime = 89;
+  ticker.innerText = 0;
+  timer.innerText = 89;
+  timeRemaining = 89;
+  matchedCards = [];
+  resetBoard();
+  hideCards();
+};
+
+function hideCards() {
+  cardsArray.forEach(card => {
+    card.classList.remove('show');
+});
 }
 
+//victory overlay + audio define/load/play
+function succsessScreen() {
+  victoryOverlay.classList.add('show');
+  win = new Audio('assets/sounds/well-done.wav');
+  win.play();
+}
 
-// (function shuffle() {
-//   cards.forEach(card => {
-//     let randomPos = Math.floor(Math.random() * 16);
-//     card.style.order = randomPos;
-//   });
-// })();
-
-
-// the main event handler
-cards.forEach(card => card.addEventListener('click', flipCard));
-
-// if (shown.length == 16) {
-//   alert('bob');
-// }
-
+(function shuffle() {
+  cards.forEach(card => {
+    let randomPos = Math.floor(Math.random() * N);
+    card.style.order = randomPos;
+  });
+})();
 
 // flip sound
 // https://freesound.org/s/240776/
@@ -138,3 +157,8 @@ cards.forEach(card => card.addEventListener('click', flipCard));
 
 // well done
 // https://freesound.org/s/436164/
+
+// www.rw-designer.com/
+
+// http://www.foopy.com/ 
+// https://www.urbanfonts.com
